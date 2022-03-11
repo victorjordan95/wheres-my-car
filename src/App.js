b9c8f624d23a6b72478c00491b3fe91b821abdcd
Victor Jordan <victor.jordan@mercadolivre.com>
2022-03-11 15:00:30 -0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import L from 'leaflet';
 import { useEffect, useState } from 'react';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
